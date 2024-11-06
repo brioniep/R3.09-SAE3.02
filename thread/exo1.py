9078b77ad449278d63abcd74910b3cb4fa7f1724
brioniep <leonard.sero@hotmail.com>
2024-11-06 11:01:40 +0100

    exo1 thread

--- thread/exo1.py
+++ thread/exo1.py
@@ -4,10 +4,20 @@ import time
 
 
 
-def message():
-    print("Je suis la thread {i}")
-    time.sleep(1)
+def message(thread_id):
+    for i in range(5):
+        print(f"Je suis la thread {thread_id}")
+        time.sleep(1)
 
-    for i in range (5)
+
+
+t1 = threading.Thread(target=message, args=(1,))
+t2 = threading.Thread(target=message, args=(2,))
+
+t1.start()
+t2.start()
+
+t1.join() # le join permet d'attendre la fin des thread avant de passer a la suite (pas utile dans cette exercice)
+t2.join()
 
 
