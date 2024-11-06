6f235d7abe258b9038488097043f1e2de543594b
brioniep <leonard.sero@hotmail.com>
2024-11-06 11:38:27 +0100

    ...

--- thread/exo1.py
+++ thread/exo1.py
@@ -1,15 +1,10 @@
 import threading
 import time
 
-
-
-
 def message(thread_id):
     for i in range(5):
         print(f"Je suis la thread {thread_id}")
         time.sleep(1)
-
-
 
 t1 = threading.Thread(target=message, args=(1,))
 t2 = threading.Thread(target=message, args=(2,))
---
+++ thread/exo2.py
@@ -0,0 +1,22 @@
+import threading
+import time
+
+def compte_a_rebours(n, thread_id):
+    for i in range(n, 0, -1):
+        print(f"thread {thread_id} : {i}")
+        time.sleep(0.5) 
+
+# Valeurs de départ différentes pour chaque thread
+n1 = 5
+n2 = 3
+
+thread1 = threading.Thread(target=compte_a_rebours, args=(n1, 1))
+thread2 = threading.Thread(target=compte_a_rebours, args=(n2, 2))
+
+thread1.start()
+thread2.start()
+
+thread1.join()
+thread2.join()
+
+
